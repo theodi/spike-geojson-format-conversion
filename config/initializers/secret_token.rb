cb4df340de9393e86facf34760120fad40b7576f
Phil Lang <plangphoto@gmail.com>
2018-07-30 14:42:41 +0100

    Initial project build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geo::Application.config.secret_token = '6f5ad9e9b5f56c2e4eae058f7dfe01bf06e51922ece0b90a9ba5cb70faca97def05468f3600698fd9da0947fad5922c9cf6d638bc358019cde485dc349d4b3ff'
