b5b60d1607f5b06e89e40a5b5efe51bd47ec149c
Phil Lang <plangphoto@gmail.com>
2018-08-01 14:02:14 +0100

    Ingest shapefile using rgeo-shapefile Gem

--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -1,4 +1,9 @@
 class ConversionController < ApplicationController
+	require 'rgeo/shapefile'
+
   def index
+		RGeo::Shapefile::Reader.open('stations.shp') do |file|
+			@file = file
+		end
   end
 end
