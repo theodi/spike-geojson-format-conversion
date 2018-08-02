e5050a6de265c93becabb43d662b6cd4b3269304
Phil Lang <plangphoto@gmail.com>
2018-08-02 10:55:28 +0100

    Basic reading of shapefile geometric data

--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -1,9 +1,18 @@
 class ConversionController < ApplicationController
 	require 'rgeo/shapefile'
+	require 'rgeo/geo_json'
 
   def index
 		RGeo::Shapefile::Reader.open('stations.shp') do |file|
-			@file = file
+			@shapefile = file
+			file.each do |record|
+		    puts "Record number #{record.index}:"
+		    puts "Geometry: #{record.geometry.as_text}"
+		    puts "Attributes: #{record.attributes.inspect}"
+		  end
+		  file.rewind
+		  record = file.next
+		  puts "First record geometry was: #{record.index}"
 		end
   end
 end
