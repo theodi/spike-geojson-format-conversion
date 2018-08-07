5cd3ddd0a80796ada181bb1aa1aecb1885ab78f4
Phil Lang <plangphoto@gmail.com>
2018-08-07 15:02:34 +0100

    Update routes and add views

--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -3,9 +3,14 @@ class ConversionController < ApplicationController
   def index
   end
 
+	def new
+		@title = 'Shapefile to geoJSON converter'
+	end
+
 	def create
 		shapefiles = params[:files]
 		@geojson = ShapefileToGeojsonService.new(shapefiles).perform
+		render :show
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Geo::Application.routes.draw do
 	resources :conversion
+
+	root to: 'conversion#new'
 end
