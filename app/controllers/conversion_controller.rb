c40692310d71417695288c3cc6d665dd2f41a5a9
Phil Lang <plangphoto@gmail.com>
2018-08-07 13:58:46 +0100

    Reactor conversion method out of controller into service

--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -1,67 +1,11 @@
 class ConversionController < ApplicationController
-	require 'rgeo/shapefile'
-	require 'rgeo/geo_json'
-	require 'json'
 
   def index
   end
 
 	def create
-		files = []
-		# Get files from form upload and write to '/public/tmp' directory
-		if !params[:files].nil?
-		  params[:files].each{ |file|
-		  	shapefile = file.tempfile.read
-				filename = file.original_filename
-				files << filename
-				File.open(File.join(Rails.root, 'public', 'tmp', filename), 'wb') { |f| f.write shapefile }
-		  }
-		end
-
-		# Get uploaded shp filename for passing to RGeo
-		shp_extract = files.select{ |i| i[/\.shp$/] }
-		shp_file = shp_extract[0]
-		path = Rails.root.join("public/tmp/#{shp_file}")
-
-		# Factory to set SRID - Is this being set correctly?
-		factory = RGeo::Geographic.spherical_factory(:srid => 4326)
-
-		# Array for holding each created 'Feature' from the Shapefile
-		features = []
-
-		# Open Shapefile with SRID factory
-		RGeo::Shapefile::Reader.open(path, :factory => factory) do |file|
-
-			# Loop through loaded shapefile to get each 'record'
-			file.each do |record|
-
-				# Factory to create a RGeo 'feature' for each record
-				factory = RGeo::GeoJSON::EntityFactory.instance
-
-				# Feature object for each Shapefile record
-				feature = factory.feature(
-					record.geometry,
-					record.index,
-					record.attributes
-				)
-
-				# Push 'feature' to array
-				features << feature
-			end
-		end
-
-		# Create a 'Feature Collection'
-		feature_collection = RGeo::GeoJSON::FeatureCollection.new(features)
-
-		# Encode 'Feature Collection' using GeoJSON Gem and convert to JSON
-		@geojson = RGeo::GeoJSON.encode(feature_collection).to_json
-
-		# Uncomment to render as JSON
-		# render json: JSON.pretty_generate(@geojson)
-
-		# Clear shp files from public/tmp
-		# S3 should be employed here for file storage
-		`rm -fr public/tmp/*`
+		shapefiles = params[:files]
+		@geojson = ShapefileToGeojsonService.new(shapefiles).perform
 	end
 
 end
--- app/services/shapefile_to_geojson_service.rb
+++ app/services/shapefile_to_geojson_service.rb
@@ -1,5 +1,82 @@
 class ShapefileToGeojsonService
-	def initialize(foo)
-		puts foo
+	require 'rgeo/shapefile'
+	require 'rgeo/geo_json'
+	require 'json'
+
+	def initialize(shapefiles)
+		@shapefiles = shapefiles
 	end
+
+	def perform
+		save_shapefiles
+		create_features_collection
+		delete_temporary_files
+		convert_to_geojson
+	end
+
+	private
+
+	# Save shapefile to 'public/tmp'
+	def save_shapefiles
+		@files = []
+
+		@shapefiles.each do |file|
+			shapefile = file.tempfile.read
+			shapefile_name = file.original_filename
+			@files << filename
+			File.open(File.join(Rails.root, 'public', 'tmp', shapefile_name), 'wb') { |f| f.write shapefile }
+		end
+	end
+
+	# Creatures features from each Shapefile object
+	def create_features_collection
+		features = []
+		
+		# Factory to set SRID - Is this being set correctly?
+		factory = RGeo::Geographic.spherical_factory(:srid => 4326)
+
+		# Open Shapefile with SRID factory
+		RGeo::Shapefile::Reader.open(get_shp_file(@files), :factory => factory) do |file|
+
+			file.each do |record|
+
+				# Factory to create a RGeo 'feature' for each record
+				factory = RGeo::GeoJSON::EntityFactory.instance
+
+				# Feature object for each Shapefile record
+				feature = factory.feature(
+					record.geometry,
+					record.index,
+					record.attributes
+				)
+
+				features << feature
+			end
+		end
+
+		@features_collection = add_features_to_collection(features)
+	end
+
+	# Clear shp files from public/tmp - Temporary measure - employ S3 for file storage
+	def delete_temporary_files
+		`rm -fr public/tmp/*`
+	end
+
+	# Encode 'Feature Collection' using GeoJSON Gem and convert to JSON
+	def convert_to_geojson
+		RGeo::GeoJSON.encode(@features_collection).to_json
+	end
+
+	# Get uploaded shp filename
+	def get_shp_file(files)
+		shp_extract = files.select{ |i| i[/\.shp$/] }
+		shp_file = shp_extract[0]
+		@path = Rails.root.join("public/tmp/#{shp_file}")
+	end
+
+	# Create a 'Features Collection' using 'Features'
+	def add_features_to_collection(features)
+		RGeo::GeoJSON::FeatureCollection.new(features)
+	end
+
 end
